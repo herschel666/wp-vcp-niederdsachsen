245c02ccc219981743ac6ea89c68a9ab7664355b
Emanuel <kraemerseele@gmx.de>
2015-01-26 21:24:43 +0100

    Removing the header image

--- header.php
+++ header.php
@@ -33,20 +33,6 @@
           <h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
         <?php endif; ?>
       </hgroup><!-- end site-title -->
-
-      <?php
-      // The header image
-      // Check if this is a post or page, if it has a thumbnail, and if it's a big one
-      if ( is_singular() &&
-        current_theme_supports( 'post-thumbnails' ) &&
-        has_post_thumbnail( $post->ID ) &&
-        ( /* $src, $width, $height */ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'post-thumbnail' ) ) &&
-        $image[1] >= HEADER_IMAGE_WIDTH ) :
-        // Houston, we have a new header image!
-        echo get_the_post_thumbnail( $post->ID , array(1102,350), array('class' => 'headerimage'));
-        elseif ( get_header_image() ) : ?>
-        <img src="<?php header_image(); ?>" class="headerimage" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" alt="" /><!-- end headerimage -->
-      <?php endif; ?>
       <div class="clear"></div>
 
     </header><!-- end header -->
